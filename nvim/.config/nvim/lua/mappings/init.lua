d8c9db2ec57605583a732055f617a0c90e4a8a99
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-12-31 22:58:15 +0545

    add a few useful remaps

--- nvim/.config/nvim/lua/mappings/init.lua
+++ nvim/.config/nvim/lua/mappings/init.lua
@@ -3,9 +3,24 @@ local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = ' '
 map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
-map('n', '<leader>p', '"*p', opts)
-map('n', '<leader>y', '"*y', opts)
-map('n', '<leader>w', ':w<CR>', opts)
-map('n', '<leader>l', ':so<CR>', opts)
 map('n', '<leader>tf', ':Telescope find_files<CR>', opts)
 map('n', '<leader>tg', ':Telescope live_grep<CR>', opts)
+
+local keymap = vim.keymap.set
+
+-- remaps
+
+--centered half page up down
+keymap('n', '<C-d>', '<C-d>zz')
+keymap('n', '<C-u>', '<C-u>zz')
+
+--centered search
+keymap('n', 'n', 'nzzzv')
+keymap('n', 'N', 'Nzzzv')
+
+-- yank put to primary register
+keymap({ 'n', 'v' }, '<leader>y', '"*y')
+keymap('n', '<leader>Y', '"*Y')
+
+keymap({ 'n', 'v' }, '<leader>p', '"*p')
+keymap({ 'n', 'v' }, '<leader>P', '"*P')
