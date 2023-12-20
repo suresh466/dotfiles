cf76c2ad02fc905b5b756a7cc75498e38c02ab30
Suresh thagunna <suresh.thagunna77@gmail.com>
2023-12-20 12:26:30 -0500

    replace on_attach function with LspAttach autocmd

--- nvim/.config/nvim/lua/lsp-config/language-servers.lua
+++ nvim/.config/nvim/lua/lsp-config/language-servers.lua
@@ -1,5 +1,59 @@
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- Setup language servers.
+local lspconfig = require('lspconfig')
 
+lspconfig.pyright.setup {}
+lspconfig.tsserver.setup {}
+-- IMPORTANT: make sure to setup neodev BEFORE lspconfig
+require('neodev').setup({
+    -- add any options here, or leave empty to use the default settings
+})
+-- example to setup sumneko and enable call snippets
+lspconfig.lua_ls.setup({
+    -- Server-specific settings. See `:help lspconfig-setup`
+    settings = {
+        Lua = {
+            completion = {
+                callSnippet = 'Replace',
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+})
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
+
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
 for type, icon in pairs(signs) do
     local hl = 'DiagnosticSign' .. type
@@ -13,72 +67,9 @@ vim.diagnostic.config({
         border = 'rounded',
     },
 })
-
---mappings
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', '<space>gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', function()
-        vim.lsp.buf.format({ async = true })
-    end, bufopts)
-end
-
-local lsp_flags = {
-    -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
-}
-
--- IMPORTANT: make sure to setup neodev BEFORE lspconfig
-require('neodev').setup({
-    -- add any options here, or leave empty to use the default settings
-})
-
--- example to setup sumneko and enable call snippets
-require('lspconfig').lua_ls.setup({
-    settings = {
-        Lua = {
-            completion = {
-                callSnippet = 'Replace',
-            },
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-})
-
-require('lspconfig')['pyright'].setup({
-    on_attach = on_attach,
-    flags = lsp_flags,
-})
--- TypeScript
-require('lspconfig').tsserver.setup({
-    on_attach = on_attach,
-    filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
-})
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
