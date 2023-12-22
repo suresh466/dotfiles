be8b4aac6f4075a167c289f4f035467770c96ea3
Suresh thagunna <suresh.thagunna77@gmail.com>
2023-12-22 17:06:47 -0500

    switch back to default quote style formatting

--- nvim/.config/nvim/lua/conform-config/init.lua
+++ nvim/.config/nvim/lua/conform-config/init.lua
@@ -1,20 +1,12 @@
-local conform = require('conform')
+local conform = require("conform")
 conform.setup({
 	formatters_by_ft = {
-		lua = { 'stylua' },
-		javascript = { 'biome' },
+		lua = { "stylua" },
+		javascript = { "biome" },
 	},
 })
 
-conform.formatters.stylua = {
-	prepend_args = { '--quote-style', 'ForceSingle' },
-}
-
-conform.formatters.biome = {
-	prepend_args = { 'format', '--quote-style', 'single' },
-}
-
-vim.api.nvim_create_autocmd('BufWritePre', {
+vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function(args)
 		conform.format({ bufnr = args.buf })
 	end,
