cdb129af9af98c7e9aceaabd86bfcd7b2ece813b
Suresh thagunna <suresh.thagunna77@gmail.com>
2024-05-22 20:02:10 -0400

    neovim: untrack lazy-lock.json; add copilot and copilot chat plugins

--- nvim/.config/nvim/lua/lazy-config/init.lua
+++ nvim/.config/nvim/lua/lazy-config/init.lua
@@ -100,4 +100,33 @@ require("lazy").setup({
 
 	--formatting
 	"stevearc/conform.nvim",
+
+	--ai
+	{
+		"zbirenbaum/copilot.lua",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					auto_trigger = true,
+					keymap = {
+						accept = "<C-h>",
+						accept_line = "<C-b>",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
 })
