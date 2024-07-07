e4550ce225ca76cbd53ea505f1f7c728123d609f
Suresh thagunna <suresh.thagunna77@gmail.com>
2024-07-06 21:35:20 -0400

    neovim(conform): automatically run slow formatters async

--- nvim/.config/nvim/lua/conform-config/init.lua
+++ nvim/.config/nvim/lua/conform-config/init.lua
@@ -1,3 +1,4 @@
+local slow_format_filetypes = {}
 local conform = require("conform")
 conform.setup({
 	formatters_by_ft = {
@@ -9,6 +10,25 @@ conform.setup({
 		python = { "ruff_format", "ruff_fix" },
 		htmldjango = { "djlint" },
 	},
+	format_on_save = function(bufnr)
+		if slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		local function on_format(err)
+			if err and err:match("timeout$") then
+				slow_format_filetypes[vim.bo[bufnr].filetype] = true
+			end
+		end
+
+		return { timeout_ms = 200, lsp_format = "fallback" }, on_format
+	end,
+
+	format_after_save = function(bufnr)
+		if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		return { lsp_format = "fallback" }
+	end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
