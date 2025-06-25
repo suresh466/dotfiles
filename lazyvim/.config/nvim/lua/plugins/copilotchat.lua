4f89de92c3ce3de53f992ea65af0034ad75f282b
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-06-25 12:07:37 -0400

    lazyvim: disable diagnostics virtual text

--- lazyvim/.config/nvim/lua/plugins/copilotchat.lua
+++ lazyvim/.config/nvim/lua/plugins/copilotchat.lua
@@ -1,7 +1,7 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   opts = {
-    model = "claude-3.7-sonnet",
+    model = "claude-3.5-sonnet",
     auto_follow_cursor = false,
     auto_insert_mode = false,
   },
--- lazyvim/.config/nvim/lua/plugins/gruvbox.lua
+++
@@ -1,4 +0,0 @@
-return {}
--- return {
---   "ellisonleao/gruvbox.nvim",
--- }
