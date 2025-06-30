1c59d5269f7bf8bb70435d7f037607a303515516
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-06-29 22:13:00 -0400

    lazyvim: disable smooth scroll

--- lazyvim/.config/nvim/lua/plugins/lspconfig.lua
+++ lazyvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -2,5 +2,9 @@ return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
     opts.diagnostics.virtual_text = false
+    opts.diagnostics.float = {
+      source = true,
+      border = "rounded",
+    }
   end,
 }
