cd6ccfa204f7a6ad83ca870b96977c8caa80684e
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-08-20 11:34:13 -0400

    feat: disable diagnostics underline

--- lazyvim/.config/nvim/lua/plugins/lspconfig.lua
+++ lazyvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,20 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    opts.diagnostics.virtual_text = false
-    opts.diagnostics.float = {
-      source = true,
-      border = "rounded",
-    }
-  end,
+  opts = {
+    diagnostics = {
+      virtual_text = false,
+      float = {
+        source = true,
+        border = "rounded",
+      },
+      underline = false,
+    },
+  },
 }
+-- use this style to update a server list for example (deep_extend only merge tables not lists )
+-- {
+--   "hrsh7th/nvim-cmp",
+--   opts = function(_, opts)
+--     table.insert(opts.sources, { name = "emoji" })
+--   end,
+-- }
