a8337e17cd56948789c621cd54596bfadb375421
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-12-19 15:06:17 -0500

    jakoolit: update

--- jkoolit/.config/hypr/UserScripts/WallpaperSelect.sh
+++ jkoolit/.config/hypr/UserScripts/WallpaperSelect.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */
 # This script for selecting wallpapers (SUPER W)
 
@@ -93,7 +93,7 @@ menu() {
       fi
       printf "%s\x00icon\x1f%s\n" "$pic_name" "$cache_preview_image"
     else
-      printf "%s\x00icon\x1f%s\n" "$(echo "$pic_name" | cut -d. -f1)" "$pic_path"
+      printf "%s\x00icon\x1f%s\n" "$pic_name" "$pic_path"
     fi
   done
 }
